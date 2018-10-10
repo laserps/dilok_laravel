f25f787f5130d87e3a72e6ae24e9ff2b7cd38248
banjongham <banjong_147@hotmail.com>
2018-10-10 09:26:30 +0700

    edit customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -62,92 +62,93 @@ class CustomerController extends Controller
             "created_at" => "2018-09-24 06:48:56",
             "updated_at" => "2018-09-24 06:48:56",
             "created_in" => "Default Store View",
-              "addresses" => [
-                array(
-                  // "id"=> 0,
-                  "customer_id"=> 5,
-                  "region"=> array(
-                    "region_code"=> "string",
-                    "region"=> "string",
-                    "region_id"=> 0,
-                    "extension_attributes"=> array(),
-                  ),
-                  "region_id"=> 0,
-                  "country_id"=> "string",
-                  "street"=> [
-                    "string"
-                  ],
-                  "company"=> "string",
-                  "telephone"=> "string",
-                  "fax"=> "string",
-                  "postcode"=> "string",
-                  "city"=> "string",
-                  "firstname"=> "string",
-                  "lastname"=> "string",
-                  "middlename"=> "string",
-                  "prefix"=> "string",
-                  "suffix"=> "string",
-                  "vat_id"=> "string",
-                  "default_shipping"=> true,
-                  "default_billing"=> true,
-                  "extension_attributes"=> array(),
-                  "custom_attributes"=> [
+              // "addresses" => [
+              //   array(
+              //     // "id"=> 0,
+              //     "customer_id"=> 5,
+              //     "region"=> array(
+              //       "region_code"=> "string",
+              //       "region"=> "string",
+              //       "region_id"=> 0,
+              //       "extension_attributes"=> array(),
+              //     ),
+              //     "region_id"=> 0,
+              //     "country_id"=> "string",
+              //     "street"=> [
+              //       "string"
+              //     ],
+              //     "company"=> "string",
+              //     "telephone"=> "string",
+              //     "fax"=> "string",
+              //     "postcode"=> "string",
+              //     "city"=> "string",
+              //     "firstname"=> "string",
+              //     "lastname"=> "string",
+              //     "middlename"=> "string",
+              //     "prefix"=> "string",
+              //     "suffix"=> "string",
+              //     "vat_id"=> "string",
+              //     "default_shipping"=> true,
+              //     "default_billing"=> true,
+              //     "extension_attributes"=> array(),
+              //     "custom_attributes"=> [
+              //       array(
+              //         "attribute_code"=> "string",
+              //         "value"=> "string"
+              //       ),
+              //     ]
+              //   )
+              // ],
+
+            'addresses' =>
+                  [
                     array(
-                      "attribute_code"=> "string",
-                      "value"=> "string"
+                    // "id" => 9,
+                    "customer_id" => 75,
+                    "region" => array(
+                      "region_code" => "TH",
+                      "region" => "Bankok",
+                      "region_id" => 0,
+                      "extension_attributes" => array(),
+                    ),
+                    "region" => "Bankok",
+                    "region_id" => 0,
+                    "country_id" => "TH",
+                    // "street" => array("street","PO Box 321"),
+                    "company" => "workbythai",
+                    "telephone" => "0857000516",
+                    // "fax" => "1",
+                    "postcode" => "10800",
+                    "city" => "Bankok",
+                    "firstname" => $request->input('firstname'),
+                    "lastname" => $request->input('lastname'),
+                    // "middlename" => "1",
+                    // "prefix" => "1",
+                    // "suffix" => "1",
+                    // "vat_id" => "1",
+                    "default_shipping" => 1,
+                    "default_billing" => 1,
+                    // "extension_attributes" => array(),
+                    // "custom_attributes" => [
+                    //     array(
+                    //       "attribute_code" => "23",
+                    //       "value" => "bbb"
+                    //     ),
+                    //   ],
+                    ),
+                  ],
+                  "disable_auto_group_change" => 0,
+                  "extension_attributes" => [
+                      array(
+                      "is_subscribed" =>  true
+                    ),
+                  ],
+                  "custom_attributes" => [
+                    array(
+                      "attribute_code" => "23",
+                      "value" => "bbb"
                     ),
                   ]
-                )
-              ],
-            // 'addresses' =>
-            //       [
-            //         array(
-            //         // "id" => 9,
-            //         "customer_id" => 75,
-            //         "region" => array(
-            //           "region_code" => "TH",
-            //           "region" => "Bankok",
-            //           "region_id" => 0,
-            //           "extension_attributes" => array(),
-            //         ),
-            //         "region" => "Bankok",
-            //         "region_id" => 0,
-            //         "country_id" => "TH",
-            //         // "street" => array("street","PO Box 321"),
-            //         "company" => "workbythai",
-            //         "telephone" => "0857000516",
-            //         // "fax" => "1",
-            //         "postcode" => "10800",
-            //         "city" => "Bankok",
-            //         "firstname" => $request->input('firstname'),
-            //         "lastname" => $request->input('lastname'),
-            //         // "middlename" => "1",
-            //         // "prefix" => "1",
-            //         // "suffix" => "1",
-            //         // "vat_id" => "1",
-            //         "default_shipping" => 1,
-            //         "default_billing" => 1,
-            //         // "extension_attributes" => array(),
-            //         // "custom_attributes" => [
-            //         //     array(
-            //         //       "attribute_code" => "23",
-            //         //       "value" => "bbb"
-            //         //     ),
-            //         //   ],
-            //         ),
-            //       ],
-            //       "disable_auto_group_change" => 0,
-            //       "extension_attributes" => [
-            //           array(
-            //           "is_subscribed" =>  true
-            //         ),
-            //       ],
-            //       "custom_attributes" => [
-            //         array(
-            //           "attribute_code" => "23",
-            //           "value" => "bbb"
-            //         ),
-            //       ]
         );
 
         // dd($customer);
